9295068a7a851f365bd9d0f627598e09422838a0
James Gunn <james@gunn.io>
2020-03-03 16:11:53 +0000

    Add tables for apprenticeship QA

---
+++ dfc-coursedirectory/Pttcd/Tables/ApprenticeshipQASubmissions.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Pttcd].[ApprenticeshipQASubmissions]
+(
+	[ApprenticeshipQASubmissionId] INT IDENTITY NOT NULL CONSTRAINT [PK_ApprenticeshipQASubmissions] PRIMARY KEY,
+	[ProviderId] UNIQUEIDENTIFIER NOT NULL,
+	[SubmittedOn] DATETIME NOT NULL,
+	[SubmittedByUserId] VARCHAR(100) NOT NULL CONSTRAINT [FK_ApprenticeshipQASubmissions_SubmittedByUser] FOREIGN KEY REFERENCES [Pttcd].[Users] ([UserId]),
+	[ProviderBriefOverview] NVARCHAR(MAX) NOT NULL,
+	[ApprenticeshipQAStatus] TINYINT NOT NULL,
+	[LastAssessedBy] VARCHAR(100) CONSTRAINT [FK_ApprenticeshipQASubmissions_LastAssessedByUser] FOREIGN KEY REFERENCES [Pttcd].[Users] ([UserId])
+)

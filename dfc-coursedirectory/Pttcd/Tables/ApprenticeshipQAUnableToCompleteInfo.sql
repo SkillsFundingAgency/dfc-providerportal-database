f3d10a2020c97c49be9dddd6ebb480ff7b8a98b4
James Gunn <james@gunn.io>
2020-03-24 18:33:54 +0000

    Add StandardName to UnableToCompleteInfo

--- dfc-coursedirectory/Pttcd/Tables/ApprenticeshipQAUnableToCompleteInfo.sql
+++ dfc-coursedirectory/Pttcd/Tables/ApprenticeshipQAUnableToCompleteInfo.sql
@@ -4,6 +4,7 @@
 	[ProviderId] UNIQUEIDENTIFIER NOT NULL CONSTRAINT [FK_ApprenticeshipQAUnableToCompleteInfo_Provider] FOREIGN KEY REFERENCES [Pttcd].[Providers] ([ProviderId]),
 	[UnableToCompleteReasons] INT NOT NULL,
 	[Comments] NVARCHAR(MAX),
+	[StandardName] NVARCHAR(MAX),
 	[AddedOn] DATETIME NOT NULL,
 	[AddedByUserId] VARCHAR(100) NOT NULL CONSTRAINT [FK_ApprenticeshipQAUnableToCompleteInfo_AddedByUser] FOREIGN KEY REFERENCES [Pttcd].[Users] ([UserId])
 )
